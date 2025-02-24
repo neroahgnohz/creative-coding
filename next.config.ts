50a67918711b2b0821916cdf38fb4b728ccc67f5
HaoRen Zhong <zhr19950529@gmail.com>
2025-02-24 00:10:47 -0500

    add base path to avoid broken imgs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/creative-coding"
 };
 
 export default nextConfig;
