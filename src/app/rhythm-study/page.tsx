6c62afd3f5a79512b5daa135586a13c1ab261916
HaoRen Zhong <zhr19950529@gmail.com>
2025-02-24 20:44:39 -0500

    fix non-stop recording

--- src/app/rhythm-study/page.tsx
+++ src/app/rhythm-study/page.tsx
@@ -1,124 +1,82 @@
 "use client"
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import loaderStyles from "../../css/loader.module.css";
 import * as Tone from "tone";
 import { useGlobalDOMEvent } from "@/hooks/dom-event";
 import { PitchDetector } from "pitchy";
 
 export default function RhythmStudy() {
+    const ANALYSER_SIZE = 4096;
     const [isRecording, setIsRecording] = useState(false);
-    let isDetecting = false;
-
+    const micRef = useRef<Tone.UserMedia | null>(null);
+    const analyserRef = useRef<Tone.Analyser | null>(null);
+    const pitchHistoryRef = useRef<number[]>([]);
+    const rafRef = useRef<number>(null);
     Tone.start();
-    const analyserResolution = 4096;
-    const analyser =  new Tone.Analyser("waveform", analyserResolution);
-    const mic = new Tone.UserMedia();
-    const pitchDetector = PitchDetector.forFloat32Array(analyser.size);
-    pitchDetector.minVolumeDecibels = -20;
+
+    useEffect(() => {
+        micRef.current = new Tone.UserMedia();
+        analyserRef.current =  new Tone.Analyser("waveform", ANALYSER_SIZE);
+        micRef.current.connect(analyserRef.current);
+
+        return () => {
+          if (micRef.current) {
+            micRef.current.close();
+            micRef.current = null;
+          }
+
+          if (rafRef.current) {
+            cancelAnimationFrame(rafRef.current);
+            rafRef.current = null;
+          }
+          analyserRef.current = null;
+          pitchHistoryRef.current = [];
+        };
+    }, []);
 
     const isSpaceKey = (event: { keyCode: number; }) => {
         return event.keyCode == 32;
     };
-
-    let pitchHistory: number[] = [];
-    const HISTORY_LENGTH = 20;
-
-    const startRecording = () => {
-        setIsRecording(true);
-        isDetecting = true;
-        pitchHistory = []
-
-        mic.open().then(() => {
-            console.log('Microphone ready');
-            mic.connect(analyser);
-            detectPitch();
-        }).catch(e => {
-            console.log('Error opening microphone:', e);
-        });
-    }
-
-    const updatePitchHistory = (pitch: number) => {
-        pitchHistory.push(pitch);
-        if (pitchHistory.length > HISTORY_LENGTH) {
-            pitchHistory.shift(); // Keep fixed window size
-        }
-    }
-
-    const calculateSlope = (pitchHistory: number[]) => {
-        const n = pitchHistory.length;
-        let sumX = 0, sumY = 0, sumXY = 0, sumXX = 0;
-        for (let i = 0; i < n; i++) {
-            sumX += i;
-            sumY += pitchHistory[i];
-            sumXY += i * pitchHistory[i];
-            sumXX += i * i;
-        }
-
-        const slope = (n * sumXY - sumX * sumY) / (n * sumXX - sumX * sumX);
-        return slope;
-    }
-
-    const hasDip = (pitchHistory: number[]) => {
-        const firstThird = pitchHistory.slice(0, Math.floor(pitchHistory.length / 3));
-        const lastThird = pitchHistory.slice(-Math.floor(pitchHistory.length / 3));
-        
-        const slopeStart = calculateSlope(firstThird);
-        const slopeEnd = calculateSlope(lastThird);
-      
-        return slopeStart < -0.2 && slopeEnd > 0.2;
-      }
-
-    const detectTone = (pitchHistory: number[]) => {
-        if (pitchHistory.length != HISTORY_LENGTH) return undefined;
-
-        const slope = calculateSlope(pitchHistory);
-        const avgPitch = pitchHistory.reduce((a, b) => a + b, 0) / pitchHistory.length;
-
-        console.log("slope " + slope);
-        console.log("avgPitch " + avgPitch);
-        if (Math.abs(slope) <= 0.2) return 1;
-
-        if (slope > 0.2) return 2;
-      
-        if (slope < -0.2) return 4;
-      
-        if (hasDip(pitchHistory)) return 3;
-      
-        return "Unknown";
-    }
-
+    
     const detectPitch = () => {
-        if (!isDetecting) return;
+        if (!analyserRef.current || !micRef.current || micRef.current.state == "stopped") return;
 
-        const values = analyser.getValue() as Float32Array;
+        const values = analyserRef.current.getValue() as Float32Array;
+
+        const pitchDetector = PitchDetector.forFloat32Array(ANALYSER_SIZE);
+        pitchDetector.minVolumeDecibels = -20;
+
         const [pitch, clarity] = pitchDetector.findPitch(values, Tone.getContext().sampleRate);
         if (clarity > 0.85 && pitch > 60 && pitch < 300) {
-            updatePitchHistory(pitch);
-            const tone = detectTone(pitchHistory);
+            pitchHistoryRef.current.push(pitch);
             console.log(`
                 Pitch: ${pitch.toFixed(1)} Hz
                 Clarity: ${(clarity * 100).toFixed(0)}%
-                Tone: ${tone}
-                `);
+                `
+            );
         }
-        requestAnimationFrame(() => detectPitch());
+        rafRef.current = requestAnimationFrame(() => detectPitch());
     }
 
-    const stopRecording = () => {
-        console.log("mic closed");
-        isDetecting = false;
-        mic.close();
-        setIsRecording(false);
-    }
-
-    useGlobalDOMEvent("keydown", (e) => {
-        if (!isSpaceKey(e) || isRecording) return;
-        startRecording();
+    useGlobalDOMEvent("keydown", async (e) => {
+        e.preventDefault();
+        if (!isSpaceKey(e) || isRecording || !micRef.current) return;
+        setIsRecording(true);
+        await micRef.current.open();
+        pitchHistoryRef.current = [];
+        detectPitch();
     });
 
-    useGlobalDOMEvent("keyup", (e) => {
-        if (!isSpaceKey(e) || !isRecording) return;
-        stopRecording();
+    useGlobalDOMEvent("keyup", async (e) => {
+        e.preventDefault();
+        if (!isSpaceKey(e) || !isRecording || !micRef.current) return;
+        micRef.current.close();
+        if (rafRef.current) {
+            cancelAnimationFrame(rafRef.current);
+        }
+        setIsRecording(false);
+
+        console.log(pitchHistoryRef.current);
     });
 
     return (
